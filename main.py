a795b2b334a877dedd02bd262ea5985046ad5321
“marcusngwj” <marcusngwj@msn.com>
2017-08-05 13:47:51 +0800

    Add on_chat_message method

--- main.py
+++ main.py
@@ -6,6 +6,20 @@ from telepot.loop import MessageLoop
 
 bot = telepot.Bot('438370426:AAG3pe5fwtKN42TqnTel3WZ-QU4LqDP0Wos')
 
+def on_chat_message(msg):
+	contentType, chatType, chatId = telepot.glance(msg)
+	
+	if contentType != 'text':
+		return
+	
+	if contentType == 'text':
+		textFromUser = msg['text']
+			
+		if textFromUser == '/start':
+			bot.sendMessage(chatId, 'hello')
+				
+
+MessageLoop(bot, {'chat': on_chat_message}).run_as_thread()
 print('Listening ...')
 
 #Keep the program running
