e1d826ea1cb43d9fd3bcc8859740be7b4e88701b
“marcusngwj” <marcusngwj@msn.com>
2017-08-05 13:33:51 +0800

    Initial Commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import time
+import threading
+import telepot
+from telepot.loop import MessageLoop
+
+bot = telepot.Bot('438370426:AAG3pe5fwtKN42TqnTel3WZ-QU4LqDP0Wos')
+
+print('Listening ...')
+
+#Keep the program running
+while 1:
+	time.sleep(10)
