d4765c27ac7e85c59c1fcbb16671233f76728ee0
“marcusngwj” <marcusngwj@msn.com>
2017-08-07 21:09:58 +0800

    Add Person Class + basic reply to 'add expense'

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import sys
 import time
 import threading
+import math
 import telepot
 from telepot.loop import MessageLoop
 from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
+from telepot.namedtuple import ForceReply
 
-iouMap = {}
+iouUsageMap = {}	#Map user to currently using iou
+iouMap = {}			#Map iou to respective message_identifier
+userMap = {}		#Map user to userid
 
 def on_chat_message(msg):
 	contentType, chatType, chatId = telepot.glance(msg)
 	userId = msg['from']['id']
-	print('User Id: ', userId)
+	print('User ID: ', userId)
+	print('Chat ID: ', chatId)
 	print('MESSAGE: ', msg, '\n')
 	
 	if contentType != 'text':
@@ -25,21 +30,35 @@ def on_chat_message(msg):
 		elif textFromUser == '/newIOU':	
 			iou = Iou(userId, chatId)
 			iou.createNewIou()
+			iouMap.update({iou.iouMsgIdf:iou})
 			
-
-			#iouMap.update({userId:iouMsgId})
+			owner = Person(userId, msg['from']['first_name'], msg['from']['last_name'])
+			userMap.update({userId:owner})
+						
 			
+		if userId in iouUsageMap:
+			iouMsgIdf = iouUsageMap[userId]
+			iou = iouMap[iouMsgIdf]
+			iou.displayText = userId
+			iou.updateDisplay()
+			iouUsageMap.pop(userId)
+  
 
 def on_callback_query(msg):
 	queryId, fromId, queryData = telepot.glance(msg, flavor='callback_query')
-	iouMsgIdf = telepot.message_identifier(msg['message'])
-
 	fromFirstName = msg['from']['first_name']
+	iouMsgIdf = telepot.message_identifier(msg['message'])
 	
+	print('From: ', fromFirstName, ' (', fromId, ')')
+	print('Query ID: ', queryId)
+	print('MsgIdf: ', iouMsgIdf, '\n')
+	
+	
+	#Switch to PM, reply bot with amount spent
 	if queryData == 'addExpense':
-		keyboard = getPublicKeyboard()
-		testMsg = bot.editMessageText(iouMsgIdf, fromFirstName, reply_markup=keyboard)
-		print('TESTMSG: ', testMsg, '\n')
+		iouUsageMap.update({fromId:iouMsgIdf})
+		
+		bot.sendMessage(fromId, 'Send me the amount you spent')	#Need to explore switch inline pm
 		
 
 def getPublicKeyboard():
@@ -49,19 +68,53 @@ def getPublicKeyboard():
 				])
 	return keyboard
 			
-
+			
+			
+			
+			
 class Iou:
-	def __init__(self, userId, chatId):
-		self.userId = userId
+	def __init__(self, ownerId, chatId):
+		self.ownerId = ownerId
 		self.chatId = chatId
 		
+		self.spenderList = {}
+		
+		self.displayText = ''
+		
 	def createNewIou(self):
-		keyboard = InlineKeyboardMarkup(inline_keyboard=[
+		self.displayText = ("A new IOU has been created\n"
+							"Click 'Add Expense' to add an amout you spent")
+		self.keyboard = InlineKeyboardMarkup(inline_keyboard=[
 						[InlineKeyboardButton(text='Share IOU', callback_data='share')],
 						[InlineKeyboardButton(text='Add expense', callback_data='addExpense')],
 					])
-		self.iouMsg = bot.sendMessage(self.chatId, 'A new IOU has been created', reply_markup=keyboard)
+		self.iouMsg = bot.sendMessage(self.chatId, self.displayText, reply_markup=self.keyboard)
 		self.iouMsgIdf = telepot.message_identifier(self.iouMsg)
+		
+	def addSpender(self, person):
+		spenderList.update({person.userId:person})
+		
+	def updateDisplay(self):
+		bot.editMessageText(self.iouMsgIdf, self.displayText, reply_markup=self.keyboard)
+	
+	
+class Person:
+	def __init__(self, userId, first_name, last_name):
+		self.userId = userId
+		self.first_name = first_name
+		self.last_name = last_name
+		
+		self.amtSpent = 0
+		self.amtPaid = 0
+		self.amtToPay = 0
+		self.amtToReceive = 0
+		
+		self.iouListOwned = {}
+		
+		self.isAddingExpense = False
+	
+	def addNewIou(iouMsgIdf, iou):
+		self.iouListOwned.update({iouMsgIdf:iou})
 	
 	
 startMessage = "To create a new IOU, enter '/newIOU'"
